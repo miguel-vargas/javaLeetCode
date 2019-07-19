62eb3e5edca31fc1b796dcaed5724ca767f4259c
Miguel <migs.a.vargas@gmail.com>
2019-07-19 17:05:01 -0400

    Problem 4 solved
    implemented the class to solve problem 4

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        LongestSubstrNoRepeatChar.runLongest();
+        MedianTwoSortedArrays.runMedianTwoArrays();
     }
 
 }
---
+++ src/MedianTwoSortedArrays.java
@@ -0,0 +1,44 @@
+/**
+ * Created by Miguel on 7/19/2019.
+ */
+public class MedianTwoSortedArrays {
+    public static void runMedianTwoArrays() {
+        int[] nums1 = new int[]{1,2};
+        int[] nums2 = new int[]{3,4};
+
+        double ans = findMedianSortedArrays(nums1, nums2);
+
+        System.out.print(ans);
+    }
+
+    public static double findMedianSortedArrays(int[] nums1,int[] nums2) {
+        double ans = 0;
+        int[] mergedArray = new int[nums1.length+nums2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+            mergedArray[k++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+        }
+
+        while (i < nums1.length) {
+            mergedArray[k++] = nums1[i++];
+        }
+
+        while (j < nums2.length) {
+            mergedArray[k++] = nums2[j++];
+        }
+
+        if (mergedArray.length % 2 == 0) {
+            ans = mergedArray[mergedArray.length/2] + mergedArray[(mergedArray.length/2)-1];
+            ans /= 2;
+
+            return ans;
+        }
+
+        int medianIndex = mergedArray.length / 2;
+
+        ans = mergedArray[medianIndex];
+        return ans;
+    }
+}
+
