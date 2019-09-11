1af5173b7ae9fe289b13e6bfcfa970398dbb75a8
Miguel Vargas <migs.a.vargas@gmail.com>
2019-09-11 10:28:36 -0400

    Solution to 10
    Included the proper algorithm and the Java string API method.

---
+++ src/RegExMatching.java
@@ -0,0 +1,34 @@
+public class RegExMatching {
+    public static void runRegExMatching(){
+        String str ="aa";
+
+        String pattern = "a";
+
+        boolean solution = isMatch(str, pattern);
+
+        System.out.println(solution);
+    }
+
+    private static boolean isMatch(String s, String p) {
+        return s.matches(p);
+    }
+
+    private static boolean isMatchDynamicProgramming(String text, String pattern) {
+        boolean[][] dp = new boolean[text.length() + 1][pattern.length() + 1];
+        dp[text.length()][pattern.length()] = true;
+
+        for (int i = text.length(); i >= 0; i--){
+            for (int j = pattern.length() - 1; j >= 0; j--){
+                boolean first_match = (i < text.length() &&
+                        (pattern.charAt(j) == text.charAt(i) ||
+                                pattern.charAt(j) == '.'));
+                if (j + 1 < pattern.length() && pattern.charAt(j+1) == '*'){
+                    dp[i][j] = dp[i][j+2] || first_match && dp[i+1][j];
+                } else {
+                    dp[i][j] = first_match && dp[i+1][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
